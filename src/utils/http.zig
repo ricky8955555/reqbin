50e5849d3db908334e1b7bd5492ac1e900e5b4b3
Ricky8955555 <rkmiao@duck.com>
2025-11-20 19:07:17 +0800

    fix(http): return immediately on untrusted proxy

--- src/utils/http.zig
+++ src/utils/http.zig
@@ -4,30 +4,30 @@ const httpz = @import("httpz");
 
 const net_utils = @import("net.zig");
 
+inline fn isTrustedProxy(address: std.net.Address, trusted_proxies: []const net_utils.Network) bool {
+    for (trusted_proxies) |network| {
+        if (network.isHost(address)) return true;
+    }
+
+    return false;
+}
+
 pub fn retrieveRemoteAddr(request: *httpz.Request, trusted_proxies: []const net_utils.Network) std.net.Address {
-    const trusted = trusted: {
-        for (trusted_proxies) |network| {
-            if (network.isHost(request.address)) break :trusted true;
-        } else break :trusted false;
-    };
+    var address = request.address;
 
-    var address: std.net.Address = request.address;
-
-    if (trusted) {
-        if (request.header("x-forwarded-for")) |x_forwarded_for| x_forwarded_for: {
+    if (isTrustedProxy(address, trusted_proxies)) trusted: {
+        if (request.header("x-forwarded-for")) |x_forwarded_for| {
             var ip_iter = std.mem.splitBackwardsScalar(u8, x_forwarded_for, ',');
 
-            while (ip_iter.next()) |ip| {
-                address = std.net.Address.parseIp(ip, 0) catch break :x_forwarded_for;
-
-                for (trusted_proxies) |network| {
-                    if (network.isHost(address)) break;
-                } else break :x_forwarded_for;
+            while (ip_iter.next()) |part| {
+                const ip = std.mem.trim(u8, part, " ");
+                address = std.net.Address.parseIp(ip, 0) catch break :trusted;
+                if (!isTrustedProxy(address, trusted_proxies)) break :trusted;
             }
         }
 
-        if (request.header("x-real-ip")) |x_real_ip| x_real_ip: {
-            address = std.net.Address.parseIp(x_real_ip, 0) catch break :x_real_ip;
+        if (request.header("x-real-ip")) |x_real_ip| {
+            address = std.net.Address.parseIp(x_real_ip, 0) catch break :trusted;
         }
     }
 
