dfff4175665ee5ffe5a7c1f11e45286b47a2b61a
Ricky8955555 <rkmiao@duck.com>
2025-11-20 18:46:40 +0800

    fix(http): check x-real-ip after x-forwarded-for all trusted

--- src/utils/http.zig
+++ src/utils/http.zig
@@ -11,27 +11,25 @@ pub fn retrieveRemoteAddr(request: *httpz.Request, trusted_proxies: []const net_
         } else break :trusted false;
     };
 
+    var address: std.net.Address = request.address;
+
     if (trusted) {
         if (request.header("x-forwarded-for")) |x_forwarded_for| x_forwarded_for: {
             var ip_iter = std.mem.splitBackwardsScalar(u8, x_forwarded_for, ',');
 
-            var addr: ?std.net.Address = null;
-            ip_iter: while (ip_iter.next()) |ip| {
-                addr = std.net.Address.parseIp(ip, 0) catch break :x_forwarded_for;
+            while (ip_iter.next()) |ip| {
+                address = std.net.Address.parseIp(ip, 0) catch break :x_forwarded_for;
 
                 for (trusted_proxies) |network| {
-                    if (network.isHost(addr.?)) break;
-                } else break :ip_iter;
+                    if (network.isHost(address)) break;
+                } else break :x_forwarded_for;
             }
-
-            if (addr) |client| return client;
         }
 
         if (request.header("x-real-ip")) |x_real_ip| x_real_ip: {
-            const addr = std.net.Address.parseIp(x_real_ip, 0) catch break :x_real_ip;
-            return addr;
+            address = std.net.Address.parseIp(x_real_ip, 0) catch break :x_real_ip;
         }
     }
 
-    return request.address;
+    return address;
 }
