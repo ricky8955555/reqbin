217487483d070556e5499e128da9b324cd09b6e4
Ricky8955555 <rkmiao@duck.com>
2026-03-25 17:15:34 +0800

    feat(network): format

--- src/network.zig
+++ src/network.zig
@@ -1,4 +1,7 @@
+const builtin = @import("builtin");
 const std = @import("std");
+
+const native_endian = builtin.target.cpu.arch.endian();
 
 pub const Ip4Network = struct {
     addr: u32,
@@ -39,6 +42,12 @@ pub const Ip4Network = struct {
 
             return Ip4Network.init(addr, 32);
         }
+    }
+
+    pub fn format(self: Ip4Network, w: *std.Io.Writer) std.Io.Writer.Error!void {
+        const nativeAddr = std.mem.bigToNative(u32, self.addr);
+        const bytes: *const [4]u8 = @ptrCast(&nativeAddr);
+        try w.print("{d}.{d}.{d}.{d}", .{ bytes[0], bytes[1], bytes[2], bytes[3] });
     }
 
     pub fn isHost(self: Ip4Network, host: std.net.Ip4Address) bool {
@@ -88,6 +97,78 @@ pub const Ip6Network = struct {
         }
     }
 
+    pub fn format(self: Ip6Network, w: *std.Io.Writer) std.Io.Writer.Error!void {
+        // This function is modified from `std.net.Ip6Address.format`.
+
+        const nativeAddr = std.mem.bigToNative(u128, self.addr);
+        const bytes: *const [16]u8 = @ptrCast(&nativeAddr);
+
+        if (std.mem.eql(u8, bytes[0..12], &[_]u8{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff })) {
+            try w.print("::ffff:{d}.{d}.{d}.{d}", .{ bytes[12], bytes[13], bytes[14], bytes[15] });
+            return;
+        }
+
+        const big_endian_parts = @as(*align(1) const [8]u16, @ptrCast(bytes));
+        const native_endian_parts = switch (native_endian) {
+            .big => big_endian_parts.*,
+            .little => blk: {
+                var buf: [8]u16 = undefined;
+                for (big_endian_parts, 0..) |part, i| {
+                    buf[i] = std.mem.bigToNative(u16, part);
+                }
+                break :blk buf;
+            },
+        };
+
+        // Find the longest zero run
+        var longest_start: usize = 8;
+        var longest_len: usize = 0;
+        var current_start: usize = 0;
+        var current_len: usize = 0;
+
+        for (native_endian_parts, 0..) |part, i| {
+            if (part == 0) {
+                if (current_len == 0) {
+                    current_start = i;
+                }
+                current_len += 1;
+                if (current_len > longest_len) {
+                    longest_start = current_start;
+                    longest_len = current_len;
+                }
+            } else {
+                current_len = 0;
+            }
+        }
+
+        // Only compress if the longest zero run is 2 or more
+        if (longest_len < 2) {
+            longest_start = 8;
+            longest_len = 0;
+        }
+
+        var i: usize = 0;
+        var abbrv = false;
+        while (i < native_endian_parts.len) : (i += 1) {
+            if (i == longest_start) {
+                // Emit "::" for the longest zero run
+                if (!abbrv) {
+                    try w.writeAll(if (i == 0) "::" else ":");
+                    abbrv = true;
+                }
+                i += longest_len - 1; // Skip the compressed range
+                continue;
+            }
+            if (abbrv) {
+                abbrv = false;
+            }
+            try w.print("{x}", .{native_endian_parts[i]});
+            if (i != native_endian_parts.len - 1) {
+                try w.writeAll(":");
+            }
+        }
+    }
+
     pub fn isHost(self: Ip6Network, host: std.net.Ip6Address) bool {
         const addr = addrToInt(host);
         return (addr & self.networkMask()) == self.addr;
@@ -101,8 +182,8 @@ pub const Network = union(enum) {
     pub fn parse(network: []const u8) !Network {
         const addr, const prefix_len = parse: {
             if (std.mem.indexOfScalar(u8, network, '/')) |slash_pos| {
-        const addr = try std.net.Address.parseIp(network[0..slash_pos], 0);
-        const prefix_len = try std.fmt.parseInt(u8, network[slash_pos + 1 ..], 10);
+                const addr = try std.net.Address.parseIp(network[0..slash_pos], 0);
+                const prefix_len = try std.fmt.parseInt(u8, network[slash_pos + 1 ..], 10);
 
                 break :parse .{ addr, prefix_len };
             } else {
@@ -122,6 +203,13 @@ pub const Network = union(enum) {
                 return .{ .ip6 = ip6_network };
             },
             else => unreachable,
+        }
+    }
+
+    pub fn format(self: Network, w: *std.Io.Writer) std.Io.Writer.Error!void {
+        switch (self) {
+            .ip4 => |network| try network.format(w),
+            .ip6 => |network| try network.format(w),
         }
     }
 
